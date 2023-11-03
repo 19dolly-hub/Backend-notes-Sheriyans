a76ddc89740a0b777613c2336d8bd287afaecbd6
19dolly-hub <dollyraghuwanshi16@gmail.com>
2023-11-03 18:33:48 +0530

    modify

--- express/fetchServer.js
+++ express/fetchServer.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = 3002;
 
 app.get('/', (req, res) => {
     res.send(`<a href="/api/fetchFact"><button>Get fact</button></a>`);
